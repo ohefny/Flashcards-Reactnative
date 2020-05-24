dca40aa089e762ea61c0a2674a43ae3eb15fc1aa
Omar El Hefny <o.hefny@gmail.com>
2020-05-24 03:03:37 +0200

    faet: add AsyncStorage implementation

---
+++ utils.js
@@ -0,0 +1,39 @@
+import { AsyncStorage } from "react-native";
+
+const DECKS_STORAGE_KEY = "DECKS_STORAGE_KEY";
+const CARDS_STORAGE_KEY = "CARDS_STORAGE_KEY";
+
+export function fetchDecks() {
+  return AsyncStorage.getItem(DECKS_STORAGE_KEY);
+}
+
+export function addDeck(deck) {//{id,title}
+  return AsyncStorage.getItem(DECKS_STORAGE_KEY).andThen((decks) => {
+    initDecks = decks == null ? {} : decks;
+    initDecks[deck.id] = deck;
+    AsyncStorage.setItem(DECKS_STORAGE_KEY, JSON.stringify(initDecks));
+  });
+}
+export function fetchCards() {
+  return AsyncStorage.getItem(CARDS_STORAGE_KEY)
+}
+
+export function addCard(card) {//{id,answer,question,deckID}
+  return AsyncStorage.getItem(CARDS_STORAGE_KEY).andThen((cards) => {
+    initCards = cards == null ? {} : cards;
+    initCards[card.id] = card;
+    AsyncStorage.setItem(CARDS_STORAGE_KEY, JSON.stringify(initCards));
+  });
+}
+
+export function removeEntry(key) {
+  return AsyncStorage.getItem(DECKS_STORAGE_KEY).then((results) => {
+    const data = JSON.parse(results);
+    data[key] = undefined;
+    delete data[key];
+    AsyncStorage.setItem(CALENDAR_STORAGE_KEY, JSON.stringify(data));
+  });
+}
+function objectToArray(obj) {
+    return Object.keys(obj).map((key) => obj[key]);
+  }
